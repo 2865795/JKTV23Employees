ff43869d7bbece00939254ea6660c873835b1f64
Pupil <Pupil@ivkhk.ee>
2024-09-30 15:31:31 +0300

    Заглушка для тестов

--- src/main/java/ee/ivkhkdev/Main.java
+++ src/main/java/ee/ivkhkdev/Main.java
@@ -4,6 +4,7 @@ package ee.ivkhkdev;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        App app = new App();
+        app.run();
     }
 }
